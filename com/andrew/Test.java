1678bffd1cbc5a3211e563954323d0d2ad47e4eb
Andrew Li <andrew.li2252@gmail.com>
2023-10-03 19:06:35 -0400

    The Hello World

---
+++ com/andrew/Test.java
@@ -0,0 +1,11 @@
+package ccc.com.andrew;
+
+import java.util.Arrays;
+
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program with these arguments: " + Arrays.toString(args));
+    }
+};
