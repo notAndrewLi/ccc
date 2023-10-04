cee6331f0db1c8ef4fac1ef02c1f6f4ae2af3fd0
Andrew Li <andrew.li2252@gmail.com>
2023-10-03 21:05:15 -0400

    Added fileReader

--- com/andrew/Test.java
+++ com/andrew/Test.java
@@ -1,11 +1,32 @@
 package ccc.com.andrew;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-class Test
-{
-    public static void main(String []args)
-    {
-        System.out.println("My First Java Program with these arguments: " + Arrays.toString(args));
+// class Test
+// {
+//     public static void main(String []args)
+//     {
+//         System.out.println("My First Java Program with these arguments: " + Arrays.toString(args));
+//     }
+// };
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class Test {
+  public static void main(String[] args) {
+    try {
+      File myObj = new File("/Users/andrew/Downloads/all_data/senior/s1j4/s1.1-01.in");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-};
+  }
+}
