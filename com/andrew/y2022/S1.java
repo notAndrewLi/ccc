2bdb102813222927b05047f7c5abd4de7980554a
Leo Li <leoligz@hotmail.com>
2023-11-09 21:39:40 -0500

    Solved S1 2022.

---
+++ com/andrew/y2022/S1.java
@@ -0,0 +1,46 @@
+package ccc.com.andrew.y2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import ccc.com.andrew.Problem;
+import ccc.com.andrew.TestCase;
+
+public class S1 implements Problem {
+    private static Path BasePath = TestCase.basePath.resolve("2022").resolve("senior").resolve("s1");
+
+    @Override
+    public ArrayList<TestCase> getTestCases() throws IOException {
+        Stream<Path> stream = Files.find(BasePath, Integer.MAX_VALUE,
+                (path, basicFileAttributes) -> path.toFile().getName().matches(
+                        "s1.*.in"));
+        try {
+            ArrayList<TestCase> testCases = new ArrayList<TestCase>();
+            stream.forEach((path) -> testCases.add(new TestCase(path, 1)));
+            return testCases;
+        } finally {
+            stream.close();
+        }
+    }
+
+    @Override
+    public ArrayList<String> run(TestCase tc) {
+        int num = Integer.valueOf(tc.In[0][0]);
+        System.out.println("Working on Test Case " + tc.InFile.getFileName().toString());
+        int answer = 0;
+        for (int i = 0; i <= num / 4; i++) {
+            if ((num - 4 * i) % 5 == 0) {
+                answer++;
+            }
+        }
+        return new ArrayList<String>(Arrays.asList(Integer.toString(answer)));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Problem.runAndCheck(new S1());
+    }
+}
